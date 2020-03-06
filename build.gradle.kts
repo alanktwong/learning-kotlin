afeebdecc5f32519f69ee603422d2057488272b6
Alan Wong <awong@tacitknowledge.com>
2020-03-06 12:21:53 +0800

    Add files to learn kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+    `maven-publish`
+    kotlin("jvm") version "1.3.61"     // <1>
+    id("org.jetbrains.dokka") version "0.10.0"
+}
+
+group = "org.awong"
+version = "0.0.1-snapshot"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("junit:junit:4.12")
+}
+
+// Configure existing Dokka task to output HTML to typical Javadoc directory
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+// Create dokka Jar task from dokka task output
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    // dependsOn(tasks.dokka) not needed; dependency automatically inferred by from(tasks.dokka)
+    from(tasks.dokka)
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            artifact(dokkaJar)
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repository")
+        }
+    }
+}
