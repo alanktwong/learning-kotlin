7d5b6ff9f23a9f3d32287417fdb650d1ae1d02a4
Alan Wong <awong@tacitknowledge.com>
2020-03-07 11:48:05 +0800

    Finish some koans

--- src/main/kotlin/org/awong/Koan.kt
+++ src/main/kotlin/org/awong/Koan.kt
@@ -1,5 +1,14 @@
 package org.awong
 
+
+data class Person(val name: String, val age: Int)
+data class Client (val personalInfo: PersonalInfo?)
+data class PersonalInfo (val email: String?)
+
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
 class Koan {
     fun joinOptions(options: Collection<String>) = options.joinToString(
             prefix = "[",
@@ -15,5 +24,23 @@ class Koan {
             foo(name = "d", number = 2, toUpperCase = true)
     )
 
-    //https://play.kotlinlang.org/koans/Introduction/Lambdas/Task.kt
+    fun containsEven(collection: Collection<Int>): Boolean = collection.any {
+        each: Int -> each % 2 == 0
+    }
+
+    val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+    fun getPattern(): String = """\d{2} ${month} \d{4}"""
+
+    fun getPeople(): List<Person> {
+        return listOf(Person("Alice", 29), Person("Bob", 31))
+    }
+
+    fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+        val personalInfo: PersonalInfo? = client?.personalInfo
+        val email: String? = personalInfo?.email
+        if (email != null && message != null) {
+            mailer.sendMessage(email, message)
+        }
+    }
 }
