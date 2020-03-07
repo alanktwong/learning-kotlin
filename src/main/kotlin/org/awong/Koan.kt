c17c5be3bca7059602f157387e025bc22cdfca4b
Alan Wong <awong@tacitknowledge.com>
2020-03-07 12:02:47 +0800

    Finish some koans

--- src/main/kotlin/org/awong/Koan.kt
+++ src/main/kotlin/org/awong/Koan.kt
@@ -1,7 +1,41 @@
 package org.awong
 
+// https://play.kotlinlang.org/koans/Introduction/Named%20arguments/Task.kt
+fun joinOptions(options: Collection<String>) = options.joinToString(
+        prefix = "[",
+        postfix = "]")
 
+// https://play.kotlinlang.org/koans/Introduction/Default%20arguments/Task.kt
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.toUpperCase() else name) + number
+
+fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+)
+
+// https://play.kotlinlang.org/koans/Introduction/Lambdas/Task.kt
+fun containsEven(collection: Collection<Int>): Boolean = collection.any {
+    each: Int -> each % 2 == 0
+}
+
+// https://play.kotlinlang.org/koans/Introduction/Strings/Task.kt
+
+val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+fun getPattern(): String = """\d{2} ${month} \d{4}"""
+
+// https://play.kotlinlang.org/koans/Introduction/Data%20classes/Task.kt
 data class Person(val name: String, val age: Int)
+
+fun getPeople(): List<Person> {
+    return listOf(Person("Alice", 29), Person("Bob", 31))
+}
+
+
+// https://play.kotlinlang.org/koans/Introduction/Nullable%20types/Task.kt
 data class Client (val personalInfo: PersonalInfo?)
 data class PersonalInfo (val email: String?)
 
@@ -9,38 +43,31 @@ interface Mailer {
     fun sendMessage(email: String, message: String)
 }
 
-class Koan {
-    fun joinOptions(options: Collection<String>) = options.joinToString(
-            prefix = "[",
-            postfix = "]")
 
-    fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
-            (if (toUpperCase) name.toUpperCase() else name) + number
-
-    fun useFoo() = listOf(
-            foo("a"),
-            foo("b", number = 1),
-            foo("c", toUpperCase = true),
-            foo(name = "d", number = 2, toUpperCase = true)
-    )
-
-    fun containsEven(collection: Collection<Int>): Boolean = collection.any {
-        each: Int -> each % 2 == 0
-    }
-
-    val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
-
-    fun getPattern(): String = """\d{2} ${month} \d{4}"""
-
-    fun getPeople(): List<Person> {
-        return listOf(Person("Alice", 29), Person("Bob", 31))
-    }
-
-    fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
-        val personalInfo: PersonalInfo? = client?.personalInfo
-        val email: String? = personalInfo?.email
-        if (email != null && message != null) {
-            mailer.sendMessage(email, message)
-        }
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    val personalInfo: PersonalInfo? = client?.personalInfo
+    val email: String? = personalInfo?.email
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
     }
 }
+
+// https://play.kotlinlang.org/koans/Introduction/Smart%20casts/Task.kt
+fun eval(expr: Expr): Int =
+        when (expr) {
+            is Num -> expr.value
+            is Sum -> eval(expr.left) + eval(expr.right)
+            else -> throw IllegalArgumentException("Unknown expression")
+        }
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+// https://play.kotlinlang.org/koans/Introduction/Extension%20functions/Task.kt
+
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
+
+data class RationalNumber(val numerator: Int, val denominator: Int)
+
