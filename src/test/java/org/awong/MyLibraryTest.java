f508ffe4fc2c25f62afec4070dec6beb6e6c49cc
Alan Wong <awong@tacitknowledge.com>
2020-03-09 16:19:48 +0800

    Make build able to use java 11

---
+++ src/test/java/org/awong/MyLibraryTest.java
@@ -0,0 +1,23 @@
+package org.awong;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * TODO figure out how to make Java & Kotlin interoperate
+ */
+public class MyLibraryTest
+{
+    @Test
+    public void testMyLanguage() {
+        assertEquals(1, 1);
+        /*
+        final var lib = new MyLibrary();
+        assertEquals("Kotlin", lib.kotlinLanguage().name)
+        assertEquals(10, lib.kotlinLanguage().hotness)
+        assertEquals("OK", lib.start())
+        */
+    }
+
+}
