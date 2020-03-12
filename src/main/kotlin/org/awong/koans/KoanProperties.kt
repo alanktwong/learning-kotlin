e28dea435b0e22ebb63a8e5b9305088fa092c777
Alan Wong <awong@tacitknowledge.com>
2020-03-12 23:02:16 +0800

    More koans

--- src/main/kotlin/org/awong/koans/KoanProperties.kt
+++ src/main/kotlin/org/awong/koans/KoanProperties.kt
@@ -1,2 +1,36 @@
 package org.awong.koans
 
+// https://play.kotlinlang.org/koans/Properties/Properties/Task.kt
+/**
+ * Add a custom setter to PropertyExample.propertyWithCounter
+ * so that the counter property is incremented every time propertyWithCounter is assigned to.
+ */
+class PropertyExample() {
+    var counter = 0
+    var propertyWithCounter: Int? = null
+        set(value) {
+            field = value
+            counter++
+        }
+}
+
+// https://play.kotlinlang.org/koans/Properties/Lazy%20property/Task.kt
+
+/**
+ * Add a custom getter to make the 'lazy' val really lazy. It
+ * should be initialized by the invocation of 'initializer()' at
+ * the moment of the first access.
+ *
+ *  You can add as many additional properties as you need.
+ *
+ *  Do not use delegated properties!
+ */
+class LazyProperty(val initializer: () -> Int) {
+    /* TODO */
+    val lazy: Int
+        get() {
+            TODO()
+        }
+}
+
+// https://play.kotlinlang.org/koans/Properties/Delegates%20examples/Task.kt
