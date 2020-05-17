39b6302117f8ed33e2a0595d9fb83076044ebb9a
Alan Wong <awong@tacitknowledge.com>
2020-05-17 21:15:05 +0800

    Learning kotlin coroutines

--- src/main/java/org/awong/koans/coroutines/CoroutineBasics.kt
+++ src/main/java/org/awong/koans/coroutines/CoroutineBasics.kt
@@ -3,7 +3,7 @@ package org.awong.koans.coroutines
 import kotlinx.coroutines.*
 
 class CoroutineBasics {
-    fun firstCoroutine() = runBlocking<Unit> { // start main coroutine
+    fun firstCoroutine() = runBlocking { // start main coroutine
         GlobalScope.launch { // launch a new coroutine in background and continue
             delay(1000L)
             println("World!")
@@ -12,12 +12,20 @@ class CoroutineBasics {
         delay(2000L)      // delaying for 2 seconds to keep JVM alive
     }
 
-    fun joinCoroutine() = runBlocking<Unit>  {
+    fun joinCoroutine() = runBlocking  {
         val job = GlobalScope.launch { // launch a new coroutine and keep a reference to its Job
             delay(1000L)
             println("World!")
         }
         println("Hello,")
         job.join() // wait until child coroutine complete
+    }
+
+    fun launchCoroutine() = runBlocking { // this: CoroutineScope
+        launch { // launch a new coroutine in the scope of runBlocking
+            delay(1000L)
+            println("World!")
+        }
+        println("Hello,")
     }
 }
--- src/test/java/org/awong/koans/coroutines/CoroutineBasicsTest.kt
+++ src/test/java/org/awong/koans/coroutines/CoroutineBasicsTest.kt
@@ -9,16 +9,23 @@ class CoroutineBasicsTest {
     val basics = CoroutineBasics()
 
     @Test
-    fun testFirstCoroutine() = runBlocking<Unit> {
+    fun testFirstCoroutine() = runBlocking {
         // here we can use suspending functions using any assertion style that we like
         basics.firstCoroutine()
         assertTrue(true, "Ran 1st coroutine")
     }
 
     @Test
-    fun testJoinCoroutine() = runBlocking<Unit> {
+    fun testJoinCoroutine() = runBlocking {
         // here we can use suspending functions using any assertion style that we like
         basics.joinCoroutine()
         assertTrue(true, "Ran join coroutine")
+    }
+
+    @Test
+    fun testLaunchCoroutine() = runBlocking {
+        // here we can use suspending functions using any assertion style that we like
+        basics.launchCoroutine()
+        assertTrue(true, "Ran launch coroutine")
     }
 }
