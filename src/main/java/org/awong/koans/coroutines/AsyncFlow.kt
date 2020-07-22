31229df67c0bae907c3dcee0daa15888ceda3dd4
Alan Wong <awong@tacitknowledge.com>
2020-07-22 10:27:37 +0800

    Touring kts coroutines

--- src/main/java/org/awong/koans/coroutines/AsyncFlow.kt
+++ src/main/java/org/awong/koans/coroutines/AsyncFlow.kt
@@ -2,13 +2,14 @@ package org.awong.koans.coroutines
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import kotlin.system.measureTimeMillis
 
 
 class AsyncFlow {
 
     private fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 
-    private fun simple(): Sequence<Int> = sequence { // sequence builder
+    private fun simpleSequence(): Sequence<Int> = sequence { // sequence builder
         for (i in 1..3) {
             Thread.sleep(100) // pretend we are computing it
             yield(i) // yield next value
@@ -17,7 +18,7 @@ class AsyncFlow {
 
     fun computeSequentially() {
         log("START: compute sequentially")
-        simple().forEach { value -> println(value) }
+        simpleSequence().forEach { value -> println(value) }
     }
 
     private suspend fun simpleUnblock(): List<Int> {
@@ -30,10 +31,10 @@ class AsyncFlow {
         simpleUnblock().forEach { value -> log(value.toString()) }
     }
 
-
     private fun simpleFlow(): Flow<Int> = flow { // flow builder
         for (i in 1..3) {
             delay(100) // pretend we are doing something useful here
+            println("Emitting $i")
             emit(i) // emit next value
         }
     }
@@ -69,5 +70,168 @@ class AsyncFlow {
         flow.collect { value -> log(value.toString()) }
     }
 
+    fun launchFlowWithTimeout() = runBlocking {
+        log("START: Launch a flow which will timeout after 250ms")
+        withTimeoutOrNull(250) {
+            simpleFlow().collect { value -> println(value) }
+        }
+        println("Done")
+    }
 
+    fun rangeAsFlow() = runBlocking {
+        log("START: Convert an integer range to a flow")
+        (1..3).asFlow().collect { value -> println(value) }
+    }
+
+
+    private suspend fun performRequest(request: Int): String {
+        log("imitate long-running asynchronous work")
+        delay(1000)
+        return "response $request"
+    }
+
+    fun mapFlow() = runBlocking {
+        (1..3).asFlow() // a flow of requests
+                .map { request -> performRequest(request) }
+                .collect { response -> log(response) }
+    }
+
+    fun transformFlow() = runBlocking {
+        (1..3).asFlow() // a flow of requests
+                .transform { request ->
+                    emit("Making request $request")
+                    emit(performRequest(request))
+                }
+                .collect { response -> log(response) }
+    }
+
+    private fun numbers(): Flow<Int> = flow {
+        try {
+            emit(1)
+            emit(2)
+            log("This line will not execute")
+            emit(3)
+        } finally {
+            log("Finally in numbers")
+        }
+    }
+
+    fun takeFew() = runBlocking {
+        numbers().take(2) // take only the first two
+                .collect { value -> println(value) }
+    }
+
+    fun mapReduce() = runBlocking {
+        val sum = (1..5).asFlow()
+                .map { it * it } // squares of numbers from 1 to 5
+                .reduce { a, b -> a + b } // sum them (terminal operator)
+        log(sum.toString())
+    }
+
+    fun sequentialFlow() = runBlocking {
+        (1..5).asFlow()
+                .filter {
+                    log("Filter $it")
+                    it % 2 == 0
+                }
+                .map {
+                    log("Map $it")
+                    "string $it"
+                }.collect {
+                    log("Collect $it")
+                }
+    }
+
+    private fun simpleContext(): Flow<Int> = flow {
+        log("Started simple flow")
+        for (i in 1..3) {
+            emit(i)
+        }
+    }
+
+    fun preserveContext() = runBlocking {
+        simpleContext().collect { value -> log("Collected $value") }
+    }
+
+    fun wrongEmission(): Flow<Int> = flow {
+        log("The WRONG way to change context for CPU-consuming code in flow builder")
+        withContext(Dispatchers.Default) {
+            for (i in 1..3) {
+                Thread.sleep(100) // pretend we are computing it in CPU-consuming way
+                emit(i) // emit next value
+            }
+        }
+    }
+
+    private fun simpleFlowOn(): Flow<Int> = flow {
+        for (i in 1..3) {
+            Thread.sleep(100) // pretend we are computing it in CPU-consuming way
+            log("Emitting $i")
+            emit(i) // emit next value
+        }
+    }.flowOn(Dispatchers.Default) // RIGHT way to change context for CPU-consuming code in flow builder
+
+    fun launchFlowOn() = runBlocking {
+        log("START: launch flow on")
+        simpleFlowOn().collect { value ->
+            log("Collected $value")
+        }
+    }
+
+    private fun buffered(): Flow<Int> = flow {
+        for (i in 1..3) {
+            delay(100) // pretend we are asynchronously waiting 100 ms
+            emit(i) // emit next value
+        }
+    }
+
+    fun unbuffered() = runBlocking {
+        log("START: launch flow without buffering")
+        val time = measureTimeMillis {
+            buffered().collect { value ->
+                delay(300) // pretend we are processing it for 300 ms
+                log(value.toString())
+            }
+        }
+        log("Collected in $time ms")
+    }
+
+    fun buffering() = runBlocking {
+        log("START: launch flow with buffering")
+        val time = measureTimeMillis {
+            // buffer emissions, don't wait
+            buffered().buffer()
+                    .collect { value ->
+                        delay(300) // pretend we are processing it for 300 ms
+                        log(value.toString())
+                    }
+        }
+        log("Collected in $time ms")
+    }
+
+    fun conflateEmissions() = runBlocking {
+        log("START: launch flow and conflate emissions")
+        val time = measureTimeMillis {
+            buffered()
+                    .conflate() // conflate emissions, don't process each one
+                    .collect { value ->
+                        delay(300) // pretend we are processing it for 300 ms
+                        log(value.toString())
+                    }
+        }
+        println("Collected in $time ms")
+    }
+
+    fun collectLatest() = runBlocking {
+        log("START: launch flow and collect latest emissions")
+        val time = measureTimeMillis {
+            buffered()
+                    .collectLatest { value -> // cancel & restart on the latest value
+                        log("Collecting $value")
+                        delay(300) // pretend we are processing it for 300 ms
+                        log("Done $value")
+                    }
+        }
+        log("Collected in $time ms")
+    }
 }
--- src/test/java/org/awong/koans/coroutines/AsyncFlowTest.kt
+++ src/test/java/org/awong/koans/coroutines/AsyncFlowTest.kt
@@ -1,8 +1,10 @@
 package org.awong.koans.coroutines
 
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 import kotlin.test.assertTrue
 import org.junit.Test
+import java.lang.IllegalStateException
 
 class AsyncFlowTest {
     private val flow = AsyncFlow()
@@ -29,5 +31,82 @@ class AsyncFlowTest {
     fun testColdFlow() = runBlocking {
         flow.launchColdFlow()
         assertTrue(true, "Use a simple reactive flow which is a cold stream")
+    }
+
+    @Test
+    fun testLaunchFlowWithTimeout() = runBlocking {
+        flow.launchFlowWithTimeout()
+        assertTrue(true, "Use a simple reactive flow which will timeout")
+    }
+
+    @Test
+    fun testLaunchFlowFromRange() = runBlocking {
+        flow.rangeAsFlow()
+        assertTrue(true, "Use a simple reactive flow which will timeout")
+    }
+
+    @Test
+    fun testMapFlow() = runBlocking {
+        flow.mapFlow()
+        assertTrue(true, "Use a simple reactive flow is mappable")
+    }
+
+    @Test
+    fun testTransformFlow() = runBlocking {
+        flow.transformFlow()
+        assertTrue(true, "Use a simple reactive flow is transformable")
+    }
+
+    @Test
+    fun testTakeFew() = runBlocking {
+        flow.takeFew()
+        assertTrue(true, "Use a simple reactive flow takes a few and terminates the flow")
+    }
+
+    @Test
+    fun testMapReduce() = runBlocking {
+        flow.mapReduce()
+        assertTrue(true, "Use a simple reactive flow that maps and then reduces")
+    }
+
+    @Test
+    fun testSequentialOperations() = runBlocking {
+        flow.sequentialFlow()
+        assertTrue(true, "Use a simple reactive flow that performs its operations sequentially")
+    }
+
+    @Test
+    fun testPreserveContext() = runBlocking {
+        flow.preserveContext()
+        assertTrue(true, "Use a simple reactive flow that preserves its context")
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun testLaunchWrongEmission() = runBlocking {
+        flow.wrongEmission().collect { value -> println(value) }
+    }
+
+    @Test
+    fun testFlowOn() = runBlocking {
+        flow.launchFlowOn()
+        assertTrue(true, "Use a simple reactive flow that switches its context")
+    }
+
+    @Test
+    fun testUnbuffered_Buffering() = runBlocking {
+        flow.unbuffered()
+        assertTrue(true, "Use a unbuffered flow that should be slower than buffering")
+
+        flow.buffering()
+        assertTrue(true, "Use a buffered flow that should be faster than not buffering")
+    }
+
+    @Test
+    fun testConflation_CollectLatest() = runBlocking {
+        flow.conflateEmissions()
+        assertTrue(true, "Use a flow that conflates emissions")
+
+        flow.collectLatest()
+        assertTrue(true, "Use a flow that collects latest emissions")
     }
 }
